a89ae0bc8db355d74f71e0c8343a8bf4e9621973
RDG-TechM <riju.dasgupta@techmahindra.com>
2024-08-02 15:36:38 +0530

    commit testing

--- agent/project_dashboard.php
+++ agent/project_dashboard.php
@@ -19,13 +19,6 @@ $outcome2 = mysqli_query($conn, $sql2);
 $skill_query = "SELECT * FROM `skillmaster`";
 $skill_outcome = mysqli_query($conn,$skill_query);
 
-$skillOptions = "";
-while ($row = mysqli_fetch_assoc($skill_outcome)) {
-$Customer = $row['SkillName'];
-$skillid = $row['SkillId'];
-$skillOptions .= "<option value='$skillid'>$Customer</option>";
-}
-
 $vertical_query = "SELECT * FROM `verticalmaster`";
 $vertical_outcome = mysqli_query($conn,$vertical_query);
 
@@ -56,7 +49,7 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
 </head>
 
 <body>
-  
+
   <div class="navbar" style="padding-bottom: 100px;">
         <div class="logo"><span style="color: white;">Tech</span> <br><span style="color: skyblue;">HireHub</span></div>
         <div class="nav-links">
@@ -113,11 +106,16 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
         </div>
         <div class="modal-body">
           <form action="Project.php" class="" method="post">
-            
+            <!-- <div class="form-group">
+              <label for="editTitle">ProjectId</label>
+              <input name="editTitle" type="editTitle" class="form-control" name="editTitle" id="editTitle" aria-describedby="emailHelp"
+                placeholder="Enter Project Name">
+            </div> -->
             <input type="hidden" name="editProjectId" id="editProjectId">
             <div class="form-group">
               <label for="editCustomerId">Customer</label>
-              
+              <!-- <input name="editCustomerId" type="editTitle" class="form-control" name="editCustomerId" id="editCustomerId" aria-describedby="emailHelp"
+                placeholder="Enter Customer Id"> -->
               <select name="editCustomerId" id="editCustomerId">
                 <option value="" disabled selected hidden>Please select CustomerId</option>
                 <?php
@@ -144,7 +142,8 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
             </div>
             <div class="form-group">
               <label for="editLocationId">Location</label>
-              
+              <!-- <input name="editLocationId" type="editTitle" class="form-control" name="editLocationId" id="editLocationId" aria-describedby="emailHelp"
+                placeholder="Enter Location Id"> -->
               <select name="editLocationId" id="editLocationId">
                 <option value="" disabled selected hidden>Please select Location</option>
                 <?php
@@ -239,20 +238,54 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
       $verticalid = $_POST['Vertical'];
 
       $IBUid = $_POST['IBU'];
-      
+
+
+
+      //.....................
+      // $locationid="SELECT `LocationId` FROM `LocationMaster` where `LocationName` like '$location'";
+      // $res5=mysqli_query($conn,$locationid);
+      // $row5=mysqli_fetch_assoc($res5);
+      // $location5=$row['LocationId'];
+      //...............
       $query5 = "SELECT * FROM `LocationMaster` WHERE `LocationName` LIKE '$location'";
-     
+      // $query6 = "SELECT * FROM `verticalmaster` WHERE `Vertical` LIKE '$vertical'";
+      // $query7 = "SELECT * FROM `IBUmaster` WHERE `IBUname` LIKE '$IBUmaster'";
       
       $result5 = mysqli_query($conn, $query5);
       $row5 = mysqli_fetch_assoc($result5);
 
+      // $result6 = mysqli_query($conn, $query6);
+      // $row6 = mysqli_fetch_assoc($result6);
+
+      // $result7 = mysqli_query($conn, $query7);
+      // $row7 = mysqli_fetch_assoc($result7);
+
+
       $locationid = $row5['LocationId'];
+
+      // $verticalid = $row6['id'];
+
+      // $IBUid = $row7['id'];
+
 
       $Startdate = $_POST['title4'];
       $Enddate = $_POST['title5'];
       $projectname = $_POST['title'];
       $skillname = $_POST['title3'];
       $requiredHeadcount = $_POST['headcount'];
+
+      // $project_query = "SELECT `ProjectId` FROM `project` WHERE `ProjectName` LIKE '$projectname'";
+      // $project_result = mysqli_query($conn,$project_query);
+      // $project_row = mysqli_fetch_assoc($project_result);
+      // $projectid = $project_row['ProjectId'];
+      // echo "Project id" . $projectid . "<br>";
+
+      // $skill_id
+      $skill_query2 = "SELECT `SkillId` from `skillmaster` WHERE `SkillName` LIKE '$skillname'";
+      $skill_result = mysqli_query($conn, $skill_query2);
+      $skill_row = mysqli_fetch_assoc($skill_result);
+      $skillid = $skill_row['SkillId'];
+      // echo "Skill id" . $skillid . "<br>";
 
       $query = "INSERT INTO `Project` (`CustomerId`,`StartDate`,`EndDate`,`Location`,`VerticalId`,`IBUId`,`ProjectName`) VALUE ('$custid','$Startdate','$Enddate','$locationid','$verticalid','$IBUid','$projectname')";
       // var_dump($query);
@@ -272,140 +305,81 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
       $project_query = "SELECT `ProjectId` FROM `project` WHERE `ProjectName` LIKE '$projectname'";
       $project_result = mysqli_query($conn,$project_query);
       $project_row = mysqli_fetch_assoc($project_result);
-      $projectid = $project_row['ProjectId'];     
-}
+      $projectid = $project_row['ProjectId'];
+      // echo "Project id" . $projectid . "<br>";
 
-    $skills = $_POST['title3'];
-    $headcounts = $_POST['headcount'];
+      $query2 = "INSERT INTO `projectskilldetails` (`project`, `skill`, `required_headcount`) VALUES ('$projectid', '$skillid', '$requiredHeadcount')";
 
-    for ($x = 0; $x < count($skills); $x++) {
-        $project_query2="INSERT INTO `projectskilldetails` (`project`, `skill`, `required_headcount`) VALUES ('$projectid', '$skills[$x]' ,'$headcounts[$x]')";
-        $project_result2 = mysqli_query($conn,$project_query2);
-          // echo($skills[$x]);
+      // var_dump($query2);
+      $result2 = mysqli_query($conn, $query2);
+
+      if (!$result2) {
+        echo mysqli_error($conn);
+        
       }
-
-    
-
     }
 
 
-  
+  }
 
   ?>
-  <div class="container">
-    
-    <form action="Project.php" class="" method="post" style="margin-top: 35px;">
-      <div class="form-group">
-        <label for="title">Project Name</label>
-        <input name="title" type="title" class="form-control" id="title" aria-describedby="emailHelp"
-          placeholder="Enter Project Name">
-      </div>
-      <div class="form-group">
-        <label for="customerid">Customer</label>
-        <select name="customerid" id="customerid">
-          <option value="" disabled selected hidden>Please select Customer</option>
+
+
+<div class="projectContainer">
+      <table class="table " id="myTable">
+        <thead>
+          <tr>
+
+            <th scope="col">ProjectId</th>
+            <th scope="col">Project</th>
+            <th scope="col">Customer</th>
+            <th scope="col">Start Date</th>
+            <th scope="col">End Date</th>
+            <th scope="col">Location</th>
+            <th scope="col">Vertical</th>
+            <th scope="col">IBU</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+
+        <b>
           <?php
-          while ($row = mysqli_fetch_assoc($outcome1)) {
-            $Customer = $row['CustomerName'];
-            echo "<option value='$Customer'>$Customer</option>";
+          // $sql = "SELECT * FROM `Project` p,`LocationMaster` l,`CustomerMaster` c, `verticalmaster` v, `IBUmaster` i WHERE p.CustomerId=c.CustomerId, p.Location=l.LocationId", ;
+          $sql="SELECT p.ProjectID, p.ProjectName, c.CustomerName, p.StartDate, p.EndDate, l.LocationName, v.Vertical, i.IBUname FROM project p JOIN customermaster c ON p.CustomerId=c.CustomerId JOIN verticalmaster v ON p.VerticalId=v.id JOIN locationmaster l ON p.Location=l.LocationId JOIN ibumaster i ON p.IBUId=i.id";
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+            $no = 0;
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+              $no++;
+              echo "<tr>
+                  <td>" . $row['ProjectID'] . "</td>
+                  <td>" . $row['ProjectName'] . "</td>
+                  <td>" . $row['CustomerName'] . "</td>
+                  <td>" . $row['StartDate'] . "</td>
+                  <td>" . $row['EndDate'] . "</td>
+                  <td>" . $row['LocationName'] . "</td>
+                  <td>" . $row['Vertical'] . "</td>
+                  <td>" . $row['IBUname'] . "</td>
+                  <td>
+                  <button class='edit btn btn-primary'>Edit</button>
+                  <button class='delete btn btn-danger' id='" . $row['ProjectID'] . "'>Delete</button></td>
+                </tr>";
+            }
+          } else {
+            echo "0 results";
           }
           ?>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="title4">Start Date</label>
-        <input name="title4" type="date" class="form-control" id="title4" aria-describedby="emailHelp"
-          placeholder="Enter Starting Date">
-      </div>
-      <div class="form-group">
-        <label for="title5">End Date</label>
-        <input name="title5" type="date" class="form-control" id="title5" aria-describedby="emailHelp"
-          placeholder="Enter Ending Date">
-      </div>
-      <div class="form-group">
-        <label for="title2">Location</label>
-        <select name="title2" id="title2">
-          <option value="" disabled selected hidden>Please select Location</option>
-          <?php
-          while ($row = mysqli_fetch_assoc($outcome2)) {
-            $Customer = $row['LocationName'];
-            echo "<option value='$Customer'>$Customer</option>";
-          }
-          ?>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="Vertical">Vertical Name</label>
-        <select name="Vertical" id="Vertical">
-          <option value="" disabled selected hidden>Please select Vertical Name</option>
-          <?php
-          while ($row = mysqli_fetch_assoc($vertical_outcome)) {
-            $Customer = $row['id'];
-            $vertiical_name=$row['Vertical'];
-            echo "<option value='$Customer'>$vertiical_name</option>";
-          }
-          ?>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="IBU">IBU Head</label>
-        <select name="IBU" id="IBU">
-          <option value="" disabled selected hidden>Please select IBU head</option>
-          <?php
-          while ($row = mysqli_fetch_assoc($IBU_outcome)) {
-            $Customer = $row['id'];
-            $IBU_name=$row['IBUname'];
-            echo "<option value='$Customer'>$IBU_name</option>";
-          }
-          ?>
-        </select>
-      </div>
-      <style>
-    .form-group {
-      margin-bottom: 15px;
-    }
-    .skill-entry {
-      display: flex;
-      align-items: center;
-      margin-bottom: 10px;
-    }
-    .skill-entry select, .skill-entry input {
-      margin-right: 10px;
-    }
-    .skill-entry button {
-      margin-left: 10px;
-    }
-  </style>
-      <div id="skillsContainer">
-      <div class="form-group skill-entry">
-        <label for="title3">Skill</label>
-        <select name="title3[]" class="skill-select">
-          <option value="" disabled selected hidden>Please select Skill</option>
-          <?php echo $skillOptions; ?>
-        </select>
-        <label for="headcount">Required Headcount</label>
-        <input type="number" name="headcount[]" class="headcount-input" min="1" placeholder="Enter headcount">
-        <button type="button" class="remove-skill-btn">Remove</button>
-      </div> 
+
+          </tbody>
+      </table>
     </div>
-    <button type="button" id="addSkillBtn">Add Skill</button>
-    <button type="submit">Submit</button>
+  </div>
 
 
-      
-          <?php
-          while ($row = mysqli_fetch_assoc($skill_outcome)) {
-            $skilll = $row['SkillName'];
-            echo "<option value='$skilll'>$skilll</option>";
-            // echo $row;
-          }
-          ?>
-       
-      <button type="submit" class="btn btn-primary">Add Project</button>
-    </form>
-    <hr style="margin-bottom: 2rem;">
 
-
+<!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
@@ -421,17 +395,23 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
   </script>
   <script>
     edits = document.getElementsByClassName("edit");
+    // console.log(edits);
     Array.from(edits).forEach((element) => {
       element.addEventListener("click", (e) => {
         console.log("edit",);
         tr = e.target.parentNode.parentNode;
+        // console.log(tr);
         ProjectId = tr.getElementsByTagName("td")[0].innerText;
         ProjectName = tr.getElementsByTagName("td")[1].innerText;
         CustomerId = tr.getElementsByTagName("td")[2].innerText;
         StartDate = tr.getElementsByTagName("td")[3].innerText;
         EndDate = tr.getElementsByTagName("td")[4].innerText;
         Location = tr.getElementsByTagName("td")[5].innerText;
+        
+        
+       
 
+        // console.log(title,description,sno);
         editCustomerId.value = CustomerId;
         editLocationId.value = Location;
         editStart.value = StartDate;
@@ -456,50 +436,6 @@ $IBU_outcome = mysqli_query($conn,$IBU_query);
 
       })
     })
-    document.getElementById('title').addEventListener('change', function() { 
-      const selectedSkills = Array.from(this.selectedOptions).map(option => option.value); 
-      const headcountFieldsDiv = document.getElementById('headcount-fields'); 
-      // Clear any existing headcount fields 
-      headcountFieldsDiv.innerHTML = ''; 
-      selectedSkills.forEach(skill => { 
-        const label = document.createElement('label'); 
-        label.innerHTML = `Headcount for ${skill}: `; 
-        const input = document.createElement('input'); 
-        input.type = 'number'; 
-        input.name = `headcount[${skill}]`; 
-        input.required = true; 
-        headcountFieldsDiv.appendChild(label); 
-        headcountFieldsDiv.appendChild(input); 
-        headcountFieldsDiv.appendChild(document.createElement('br')); 
-      }); 
-    }); 
-    </script>
-    <script>
-    const skillOptions = `<?php echo $skillOptions; ?>`;
-
-    document.getElementById('addSkillBtn').addEventListener('click', function() {
-      const skillEntryTemplate = `
-        <div class="form-group skill-entry">
-          <label for="title3">Skill</label>
-          <select name="title3[]" class="skill-select">
-            <option value="" disabled selected hidden>Please select Skill</option>
-            ${skillOptions}
-          </select>
-          <label for="headcount">Required Headcount</label>
-          <input type="number" name="headcount[]" class="headcount-input" min="1" placeholder="Enter headcount">
-          <button type="button" class="remove-skill-btn">Remove</button>
-        </div>
-      `;
-
-      const skillsContainer = document.getElementById('skillsContainer');
-      skillsContainer.insertAdjacentHTML('beforeend', skillEntryTemplate);
-    });
-
-    document.getElementById('skillsContainer').addEventListener('click', function(event) {
-      if (event.target.classList.contains('remove-skill-btn')) {
-        event.target.parentElement.remove();
-      }
-    });
   </script>
   <script src="script/script.js"></script>
   <!-- sushanta -->
